59712b3dadc7667cda24dc022bdf2b896f5f1713
Nathan Mazzapica <johndoe@example.com>
2026-02-23 16:54:02 -0500

    feat: method for converting Job to dict

--- src/models/job.py
+++ src/models/job.py
@@ -21,3 +21,13 @@ class Job:
                 attempt_count=row[4],
                 last_locked=row[5],
                 )
+
+    def to_dict(self):
+        return {
+                "id": self.id,
+                "status": self.status,
+                "input_url": self.input_url,
+                "output_url": self.output_url,
+                "attempt_count": self.attempt_count,
+                "last_locked": self.last_locked
+                }
