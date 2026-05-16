00b43c87a6fe2c9e2c32bd2ed7c5508922c2b7da
Nathan Mazzapica <natemaz513@gmail.com>
2026-05-16 14:58:36 -0400

    refactor: move image validation to storage

--- src/storage/storage.py
+++ src/storage/storage.py
@@ -4,6 +4,10 @@ import os
 import pyvips
 from PIL import Image, ImageFile
 
+from models.image_format import ImageFormat
+from service.conversion_exceptions import InvalidImageFormatError
+
+
 class Storage(ABC):
     @staticmethod
     def _clean_filepath(filepath: str) -> str:
@@ -15,6 +19,10 @@ class Storage(ABC):
         if last_dot == -1 or last_dot < last_sep:
             return filepath
         return filepath[:last_dot]
+
+    @abstractmethod
+    def _validate_mime_type(self, filepath: str) -> str:
+        pass
 
     @abstractmethod
     def generate_preprocess_path(self, input_path: str) -> str:
@@ -31,10 +39,8 @@ class Storage(ABC):
     # I'm not sure what this will look like yet...
     # If we're getting a file from S3 it needs to be downloaded first
     # If we're getting a file locally, we just need to open it?
-    # Do we want to move image opening to this method instead of using Image.open in services? maybe... but
-    # pyvips doesn't really work like that it seems
     @abstractmethod
-    def open_image(self, filepath: str) -> bytes:
+    def open_image(self, filepath: str) -> pyvips.Image:
         pass
 
     @abstractmethod
@@ -54,9 +60,29 @@ class LocalStorage(Storage):
     def get_tmp_path(self) -> str:
         return os.path.join(self.base_path, "tmp")
 
-    # authority decide who
-    def __get_mime_type(self, input_path: str) -> str:
-        raise NotImplementedError
+    def _validate_mime_type(self, filepath: str) -> ImageFormat:
+        """Verifies the image has a valid file format and returns the format.
+
+            Raises:
+                InvalidImageFormatError if file format is not a whitelisted image.
+        """
+        with (open(self.resolve(filepath), "rb")) as f:
+            header = f.read(16)
+            if header.startswith(b"\x89PNG\r\n\x1a\n"):
+                return ImageFormat.PNG
+
+            if header[:3] == b"\xFF\xD8\xFF":
+                return ImageFormat.JPEG
+
+            if header.startswith(b"RIFF") and header[8:12] == b"WEBP":
+                return ImageFormat.WEBP
+
+            if len(header) >= 12 and header[4:8] == b"ftyp":
+                brand = header[8:12]
+                if brand in {b"heic", b"heix", b"mif1", b"msf1", b"heif"}:
+                    return ImageFormat.HEIF
+
+            raise InvalidImageFormatError
 
     def generate_preprocess_path(self, input_path: str) -> str:
         return os.path.join(self.get_tmp_path(), f"{Storage._clean_filepath(input_path)}-pre.webp")
@@ -64,10 +90,13 @@ class LocalStorage(Storage):
     def generate_output_path(self, input_path: str) -> str:
         return os.path.join(self.get_tmp_path(), f"{Storage._clean_filepath(input_path)}-out.webp")
 
-    def open_image(self, filepath: str) -> bytes:
-        with open(self.resolve(filepath), "rb") as f:
-            file_bytes = f.read()
-            return file_bytes
+    def open_image(self, filepath: str) -> pyvips.Image:
+        """Opens an image file and returns the bytes"""
+        self._validate_mime_type(filepath)
+        return pyvips.Image.new_from_file(self.resolve(filepath))
+
+
+
 
     def upload(self, filepath: str, image: pyvips.Image, pre=False) -> str | None:
         try:
