917716bfeaab368604457e0833b6ad6325f1fe68
Nathan Mazzapica <natemaz513@gmail.com>
2026-05-18 18:51:02 -0400

    feat: preprocessing in service; clean up responsibilities

--- src/service/image_processing_service.py
+++ src/service/image_processing_service.py
@@ -1,12 +1,16 @@
 import os
 import uuid
 
+import pyvips
+
+from src.service.exceptions import JobFailedError
+from src.models.job import Job
 from src.models.status import JobStatus
-from repository.repository import JobRepository
-from service.background_remover import BackgroundRemover
-from service.conversion_exceptions import InvalidImageFormatError
-from service.image_converter import ImageConverter
-from storage.storage import Storage
+from src.repository.repository import JobRepository
+from src.service.background_remover import BackgroundRemover
+from src.service.conversion_exceptions import InvalidImageFormatError
+from src.service.image_converter import convert
+from src.storage.storage import Storage
 
 
 def _final_path(img_uuid: str) -> str:
@@ -21,23 +25,37 @@ def _original_path(img_uuid: str) -> str:
     return f"uploads/original/{img_uuid}"
 
 
+def _strip_path(path: str) -> str:
+    return path.split("/")[-1]
+
+
+def _is_supported_header(h: bytes) -> bool:
+    if h.startswith(b"\x89PNG\r\n\x1a\n"):
+        return True
+    if h[:3] == b"\xFF\xD8\xFF":
+        return True
+    if h.startswith(b"RIFF") and h[8:12] == b"WEBP":
+        return True
+
+    if len(h) >= 12 and h[4:8] == b"ftyp":
+        brand = h[8:12]
+        return brand in {b"heic", b"heix", b"mif1", b"msf1", b"heif"}
+
+    return False
+
+
 class ImageProcessingService:
     MAX_UPLOAD_SIZE = 1024 * 1024 * 25
+
     def __init__(self,
                  storage: Storage,
-                 converter: ImageConverter,
                  background_remover: BackgroundRemover,
                  repository: JobRepository):
         self.storage = storage
-        self.converter = converter
         self.background_remover = background_remover
         self.repository = repository
 
-        os.makedirs("uploads/original", exist_ok=True)
-        os.makedirs("uploads/preprocessed", exist_ok=True)
-        os.makedirs("uploads/final", exist_ok=True)
-
-    def _validate_upload(self, img: bytes):
+    def _validate_upload(self, img: bytes) -> None:
         """
         Validates the uploaded image to ensure its size and format meet the requirements.
 
@@ -56,21 +74,17 @@ class ImageProcessingService:
         if len(img) > self.MAX_UPLOAD_SIZE:
             raise InvalidImageFormatError("Image is too large")
 
-        header = img[:16]
-        if (header.startswith(b"\x89PNG\r\n\x1a\n") or
-            header[:3] == b"\xFF\xD8\xFF" or
-            header.startswith(b"RIFF") and header[8:12] == b"WEBP"):
-            return
-
-        if len(header) >= 12 and header[4:8] == b"ftyp":
-            brand = header[8:12]
-            if brand in {b"heic", b"heix", b"mif1", b"msf1", b"heif"}:
-                return
+        if not _is_supported_header(img[:16]):
+            raise InvalidImageFormatError("Unsupported image format")
 
 
-        raise InvalidImageFormatError("Invalid image format")
+        try:
+            i: pyvips.Image = pyvips.Image.new_from_buffer(img, "", access="sequential")
+            i.avg()
+        except pyvips.error.Error as e:
+            raise InvalidImageFormatError("Invalid image format") from e
 
-    def submit_upload(self, img: bytes):
+    def submit_upload(self, img: bytes) -> str:
         """
         Performs basic mime type validation, saves the image to storage and creates a job in the database.
 
@@ -80,7 +94,11 @@ class ImageProcessingService:
         Raises:
             InvalidImageFormatError: if the image is not a valid image format
         """
-        self._validate_upload(img)
+        try:
+            self._validate_upload(img)
+        except InvalidImageFormatError as e:
+            raise JobFailedError("Invalid image") from e
+
         img_uuid = str(uuid.uuid4())
         path = _original_path(img_uuid)
         job_id = self.repository.create(path)
@@ -91,10 +109,21 @@ class ImageProcessingService:
             self.storage.upload_bytes(path, img)
         except OSError as e:
             self.repository.update_status(job_id, JobStatus.FAILED)
-            raise e
+            raise JobFailedError("Failed to upload image") from e
 
-    def run_preprocessing(self, job_id: int):
-        raise NotImplementedError
+        return path
+
+    def run_preprocessing(self, job: Job) -> str:
+        img = self.storage.open_bytes(job.input_url)
+        converted_img = convert(img)
+        path = _preprocessed_path(_strip_path(job.input_url))
+        try:
+            self.storage.upload_bytes(path, converted_img)
+        except OSError as e:
+            self.repository.update_status(job.id, JobStatus.FAILED)
+            raise e
+        self.repository.update_proc_url(job.id, path)
+        return path
 
     def run_background_removal(self, job_id: int):
         raise NotImplementedError
