7c153bfac1a5164426e24f96cc9c63f4aa6bb7a7
Nathan Mazzapica <natemaz513@gmail.com>
2026-05-23 14:08:54 -0400

    ref: make mock warning more attention grabbing

--- src/security/mock_virus_scanner.py
+++ src/security/mock_virus_scanner.py
@@ -1,3 +1,4 @@
+import logging
 import warnings
 from typing import Optional
 
@@ -5,7 +6,14 @@ from src.security.virus_scanner import VirusScanner
 
 class MockVirusScanner(VirusScanner):
     def __init__(self):
-        warnings.warn("Using mock virus scanner", RuntimeWarning, stacklevel=2)
+        RED = "\033[91m"
+        YELLOW = "\033[93m"
+        BOLD = "\033[1m"
+        RESET = "\033[0m"
+        logging.basicConfig(format=f"{RED}{BOLD}[%(asctime)s] {YELLOW}%(levelname)s{RESET} %(message)s")
+        logging.critical("[-] WARNING: Using mock virus scanner")
+        logging.critical("[-] WARNING: This is not a production environment")
+        logging.critical("[-] WARNING: Do not use this in production")
 
     def scan(self, stream: bytes) -> Optional[str]:
         return None
